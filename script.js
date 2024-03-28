ddb3d0631a5be009b297f071bc90a8d3e714dfe8
Lucas Lessa Gomes <105132690+LLucasLessa@users.noreply.github.com>
2024-03-28 15:00:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById("form");
+const itens = document.getElementById("listItens");
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let name = form.elements["nameProduct"].value;
+  let description = form.elements["descriptionProduct"].value;
+  let disp = form.elements["avaliableProduct"].value;
+  let price = form.elements["priceProduct"].value;
+  
+  if (disp == "sim"){
+    itens.innerHTML += `<div class="item"><div class="div-itemdesc"><p><strong>Nome do Item:</strong> ${name}</p><p><strong>Preço:</strong> ${price}</p><p><strong>Disponivel:</strong> <span style="color: green">${disp}</span></p></div><hr><p><strong>Descrição:</strong> ${description}</p></div>`;
+  }else{
+    itens.innerHTML += `<div class="item"><div class="div-itemdesc"><p><strong>Nome do Item:</strong> ${name}</p><p><strong>Preço:</strong> ${price}</p><p><strong>Disponivel:</strong> <span style="color: red">${disp}</span></p></div><hr><p><strong>Descrição:</strong> ${description}</p></div>`;
+  }
+
+})
